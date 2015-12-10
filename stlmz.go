1277e31c7f973d147282cd7b8108d0a06b00cbf8
LMZStone <t22960st@gmail.com>
2015-12-11 07:41:09 +0800

    test connect

--- stlmz.go
+++ stlmz.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"runtime"
 )
 
 var over chan bool
 var work Work
 var concurrents chan bool
+
+//http://stackoverflow.com/questions/19253469/make-a-url-encoded-post-request-using-http-newrequest
 
 func main() {
 	params := Params{}
@@ -17,6 +20,24 @@ func main() {
 	fmt.Println(params)
 
 	go work.Monitor(params)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(params.method, params.url, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		if res.StatusCode == 200 {
+			work.Successful += 1
+		} else {
+			work.Failed += 1
+		}
+	}
+
 	<-over
 }
 
--- work.go
+++ work.go
@@ -7,6 +7,8 @@ import (
 	"time"
 )
 
+var maxRecvSize int
+
 type Work struct {
 	Transactions    int
 	Availability    float32
@@ -41,6 +43,7 @@ func (w *Work) Monitor(params Params) {
 	defer func() {
 		w.ElapsedTime = time.Now().Sub(begin)
 		fmt.Println(w.ElapsedTime)
+		w.Dump()
 		over <- true
 	}()
 
@@ -52,4 +55,20 @@ func (w *Work) Monitor(params Params) {
 			return
 		}
 	}
+}
+
+func (w *Work) Dump() {
+	fmt.Println(fmt.Sprintf("%s%d%s", "Transactions:                   ", w.Transactions, " hits"))
+	fmt.Println(fmt.Sprintf("%s%5.2f%s", "Availability:                   ", w.Availability*100, " %"))
+	fmt.Println(fmt.Sprintf("%s%7.2f%s", "Elapsed time:                ", w.ElapsedTime.Seconds(), " secs"))
+	fmt.Println(fmt.Sprintf("%s%d%s", "Document length:               ", maxRecvSize, " Bytes"))
+	fmt.Println(fmt.Sprintf("%s%8.2f%s", "TotalTransfer:              ", w.TotalTransfer, " MB"))
+	fmt.Println(fmt.Sprintf("%s%7.2f%s", "Transaction rate:            ", w.TransactionRate, " trans/sec"))
+	fmt.Println(fmt.Sprintf("%s%5.2f%s", "Throughput:                    ", w.Throughput, " MB/sec"))
+	fmt.Println(fmt.Sprintf("%s%d%s", "Successful:                     ", w.Successful, " hits"))
+	fmt.Println(fmt.Sprintf("%s%d%s", "Failed:                           ", w.Failed, " hits"))
+	fmt.Println(fmt.Sprintf("%s%8.3f%s", "TransactionTime:            ", w.TransactionTime.Seconds()*1000, " ms(mean)"))
+	fmt.Println(fmt.Sprintf("%s%8.3f%s", "ConnectionTime:             ", w.ConnectionTime.Seconds()*1000, " ms(mean)"))
+	fmt.Println(fmt.Sprintf("%s%8.3f%s", "ProcessTime:                ", w.ResponseTime.Seconds()*1000, " ms(mean)"))
+	fmt.Println(fmt.Sprintf("%s%d%s", "StateCode:                    ", w.StateCode, "(code 200)"))
 }
